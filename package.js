55c4acb9e706fb94a580241dd45b1035da9da1a4
herrhelms <dev@straw.io>
2014-12-15 22:30:43 +0000

    changed version constraint of momentjs:moment

--- package.js
+++ package.js
@@ -7,7 +7,7 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0.1');
-  api.use(['templating','spacebars','underscore','momentjs:moment','mrt:underscore-string-latest@2.3.3'],'client');
+  api.use(['templating','spacebars','underscore','momentjs:moment@2.8.4','mrt:underscore-string-latest@2.3.3'],'client');
   api.addFiles(['meteor-gridforms.css','meteor-gridforms.html','meteor-gridforms.js'],'client');
 });
 
