f62f669ff3a51e9a165168d062e6a85924e1ac37
herrhelms <dev@straw.io>
2014-12-16 17:08:49 +0000

    version number updated to 1.0.1

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'herrhelms:meteor-gridforms',
   summary: 'Gridforms by @kumailht ported to meteor',
-  version: '1.0.0',
+  version: '1.0.1',
   git: 'https://github.com/herrhelms/meteor-gridforms.git'
 });
 
